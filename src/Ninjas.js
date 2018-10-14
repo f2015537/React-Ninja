d60b4d176fbe9d9ca8526ea5295e26479eea1bc8
f2015537 <divyampatro1997@gmail.com>
2018-10-15 01:49:52 +0530

    Removed age limit for ninja

--- src/Ninjas.js
+++ src/Ninjas.js
@@ -4,7 +4,7 @@ import './Ninjas.css'
 const Ninjas = ({ninjas, deleteNinja}) => {
 
   const ninjaList = ninjas.map(ninja => {
-    if (ninja.age > 20){
+    
       return (
         <div className="ninja" key={ninja.id}>
           <div>Name: { ninja.name }</div>
@@ -12,10 +12,8 @@ const Ninjas = ({ninjas, deleteNinja}) => {
           <div>Belt: { ninja.belt }</div>
           <button onClick={() => {deleteNinja(ninja.id)}}>Delete ninja</button>
         </div>
-      )
-    } else {
-      return null
-    }
+      );
+
   });
 
   return (
